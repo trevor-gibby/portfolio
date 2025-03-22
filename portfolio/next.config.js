061570ddce38f6639c1e76c04227765d06920675
Trevor Gibby <trevor@ninthroot.com>
2025-03-22 13:37:25 -0500

    Trying some more

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  env: {
+    MY_EMAIL: process.env.MY_EMAIL,
+    SMTP_EMAIL: process.env.SMTP_EMAIL,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+    SMTP_APP_PASSWORD: process.env.SMTP_APP_PASSWORD,
+
+    SESSION_TOKEN: process.env.SESSION_TOKEN,
+  },
 }
 
 module.exports = nextConfig
